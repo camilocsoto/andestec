c186a3cac9db872a13652b8d1756563fb799522b
Juan Camilo Suárez <ccamsoto@gmail.com>
2024-08-07 15:56:42 -0500

    🟢 the unit test of functions to get the token and the data  give the response 200, so you should try it and return that and also create the conection to the database

--- andes/api/utils.py
+++ andes/api/utils.py
@@ -25,3 +25,22 @@ def get_access_token():
     data = response.json()
     return data.get('access_token')
 
+def getSingleDeviceDatas():
+    url = 'https://app.dtuip.com/api/device/getSingleDeviceDatas'
+    headers = {
+        'tlinkAppId': os.getenv('TLINKAPPID'),
+        'Authorization': f"bearer {get_access_token()}" # 🟠 Bearer may change in the future.
+    }
+    body = {
+        "userId": os.getenv('USERID'),
+        "deviceNo": "YAVMMQYKDANVXPYU", # 🟠🦠 this value should change dynamically
+        "currPage": 1,
+        "pageSize": 10
+    }
+
+    response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
+
+    data = response.json()
+    return data
+
