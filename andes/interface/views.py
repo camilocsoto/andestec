3e26e2c4b4ce184dda7b21697471b7bbc6780e54
Juan Camilo Suárez <ccamsoto@gmail.com>
2024-08-14 16:31:53 -0500

    dynamic data

--- andes/interface/views.py
+++ andes/interface/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from dashboard.models import Variable
 
 # Create your views here.
 def view_index(request):
-    return render(request, 'dashboard/index.html')
+    #send information to dashboard filter each device 🔥
+    get_last_record = Variable.objects.latest('id')
+    # instance each data:
+    context = {
+        'capacidad': get_last_record.var_capacity, #current % of capacity
+        'temperatura': get_last_record.var_temperature,
+        'presion': get_last_record.var_presure,
+        'bateria': get_last_record.var_battery,
+        'senial': get_last_record.var_radiofrecuency,
+        'hora': get_last_record.var_time,
+    }
+    
+    return render(request, 'dashboard/index.html', context)
