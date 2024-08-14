5e8aaab7a15875e59cbb1dcdbca00f9febab4a45
Juan Camilo Suárez <ccamsoto@gmail.com>
2024-08-14 13:51:33 -0500

    stable version

--- andes/interface/views.py
+++ andes/interface/views.py
@@ -3,8 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def view_index(request):
     return render(request, 'dashboard/index.html')
-"""
-def signin_view(request):
-    # lógica de la vista de inicio de sesión
-    return render(request, 'signin.html')
-    """
